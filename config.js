d8a3d66be79ff9219d3a685ffb3d114745030481
HerrSkull <thomas@hth.at>
2022-04-25 04:06:30 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const API_ADDRESS = "https://mainnet-node.tanglebay.com/api/v1/addresses/";
+const BOT_TOKEN = "";
+const TREASURY_HOTWALLET = "";
+const TREASURY_ADDRESS = "";
+const COLLECTION_ID = '';
+const ROLE_ID = "";
+const GUILD_ID = "";
+const GRANT_ROLES_TO_NFT_HOLDERS = true;
+const SHOW_TREASURY_INFO = true;
+
+export {API_ADDRESS};
+export {BOT_TOKEN};
+export {TREASURY_HOTWALLET};
+export {TREASURY_ADDRESS};
+export {COLLECTION_ID};
+export {ROLE_ID};
+export {GUILD_ID};
+export {GRANT_ROLES_TO_NFT_HOLDERS};
+export {SHOW_TREASURY_INFO};

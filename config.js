fadece6c7a370dc4f6427bfb2703e7f5ea01fcbd
HerrSkull <thomas@hth.at>
2022-04-25 04:18:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const API_ADDRESS = "https://mainnet-node.tanglebay.com/api/v1/addresses/";
-const BOT_TOKEN = "";
-const TREASURY_HOTWALLET = "";
-const TREASURY_ADDRESS = "";
-const COLLECTION_ID = '';
-const ROLE_ID = "";
-const GUILD_ID = "";
-const GRANT_ROLES_TO_NFT_HOLDERS = true;
-const SHOW_TREASURY_INFO = true;
+const BOT_TOKEN = "YOUR_DISCORD_API_TOKEN";
+const TREASURY_HOTWALLET = "1st_WALLET_TO_TRACK";
+const TREASURY_ADDRESS = "2nd_WALLET_TO_TRACK";
+const COLLECTION_ID = "YOUR_NFT_COLLECTION_ID";
+const ROLE_ID = "ID_OF_ROLE_YOU_WANT_TO_GRANT";
+const GUILD_ID = "ID_OF_YOUR_DISCORD_SERVER";
+const GRANT_ROLES_TO_NFT_HOLDERS = true; // activate NFT->Role Feature
+const SHOW_TREASURY_INFO = true; // activate to track treasury wallets
 
 export {API_ADDRESS};
 export {BOT_TOKEN};
